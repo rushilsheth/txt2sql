de9cfbbf30bb8906d536d98e4e9c2a99d31f4e06
rushilsheth <sheth.rushil@gmail.com>
2025-05-04 17:20:24 -0700

    chore: remove comments

--- src/text_to_sql/agent/llm_agents.py
+++ src/text_to_sql/agent/llm_agents.py
@@ -49,7 +49,6 @@ class LLMQueryUnderstandingAgent(QueryUnderstandingAgent):
         """
         super().__init__(name, config)
         self.llm_engine = llm_engine
-        logger.info("Created LLMQueryUnderstandingAgent")
     
     def process(self, context: AgentContext) -> AgentContext:
         """
